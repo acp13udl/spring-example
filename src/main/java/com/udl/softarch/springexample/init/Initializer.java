e46b81629245ade2f718ee3110c31fc8fec15b1f
davidkaste <d.castella.85@gmail.com>
2015-02-22 18:48:26 +0100

    Cleaned code in Initializer.java

--- src/main/java/com/udl/softarch/springexample/init/Initializer.java
+++ src/main/java/com/udl/softarch/springexample/init/Initializer.java
@@ -1,23 +1,21 @@
 package com.udl.softarch.springexample.init;
 
-import javax.servlet.*;
-import javax.servlet.ServletRegistration.Dynamic;
-
 import com.udl.softarch.springexample.config.GreetingAppContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import java.util.EnumSet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
 
 public class Initializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
 
-        /*AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
         ctx.register(GreetingAppContext.class);
 
         ctx.setServletContext(servletContext);
@@ -26,15 +24,7 @@ public class Initializer implements WebApplicationInitializer {
         servlet.addMapping("/");
         servlet.setLoadOnStartup(1);
 
-        servletContext.addListener(new ContextLoaderListener(ctx));*/
-
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(GreetingAppContext.class);
-        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(rootContext));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
-
-        servletContext.addListener(new ContextLoaderListener(rootContext));
+        servletContext.addListener(new ContextLoaderListener(ctx));
     }
 
 }
