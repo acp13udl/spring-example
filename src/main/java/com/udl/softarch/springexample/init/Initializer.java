da75df5738a60ca64e87221bab6046a203dae912
davidkaste <d.castella.85@gmail.com>
2015-03-16 16:08:39 +0100

    Optimize imports

--- src/main/java/com/udl/softarch/springexample/init/Initializer.java
+++ src/main/java/com/udl/softarch/springexample/init/Initializer.java
@@ -1,19 +1,16 @@
 package com.udl.softarch.springexample.init;
 
-import javax.servlet.*;
-import javax.servlet.ServletRegistration.Dynamic;
-
 import com.udl.softarch.springexample.config.GreetingAppContext;
 import com.udl.softarch.springexample.filters.SimpleCORSFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import java.util.EnumSet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 public class Initializer implements WebApplicationInitializer {
 
